3e1569814ab4c910aec56f74bd83a4a21e6e6d4d
Er. Himanshu Raj <96337118+raj04h@users.noreply.github.com>
2024-12-18 23:26:13 +0530

    Update gem.py

--- NLP_Projects/Luna_AI/gem.py
+++ NLP_Projects/Luna_AI/gem.py
@@ -1,6 +1,6 @@
 import google.generativeai as genai
 
-API_KEY = "AIzaSyB5YhK-9aAklDGJy0xxjg8mVm1FVJJBYy4"
+API_KEY = ""# Use here yoour key
 
 def configure_api():
     genai.configure(api_key=API_KEY)
